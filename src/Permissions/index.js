ee796a28afdc7be4599cdc8d4230554071c55c61
TSENG-TZU-YU <46403343+TSENG-TZU-YU@users.noreply.github.com>
2022-12-29 13:19:52 +0800

    permision

--- src/Permissions/index.js
+++ src/Permissions/index.js
@@ -1,46 +1,111 @@
-import React from 'react';
-import './index.scss';
+import React, { useEffect, useState } from 'react';
+import './_index.scss';
+
+import CategoryFilter from './Component/CategoryFilter.js';
+import PermissionFilter from './Component/PermissionFilter.js';
+import UserFilter from './Component/UserFilter.js';
+import axios from 'axios';
+
 function Permissions() {
+  const [user, setUser] = useState([]);
+  const [nowUser, setNowUser] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [nowCategory, setNowCategory] = useState([]);
+  const [nowPermission, setNowPermission] = useState([]);
+  const [permission, setPermission] = useState([]);
+
+  useEffect(() => {
+    async function getCategory() {
+      try {
+        let res = await axios.get(
+          ' http://localhost:3001/api/permissions/category'
+        );
+
+        setCategory(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    async function getUsers() {
+      try {
+        let res = await axios.get('http://localhost:3001/api/permissions/user');
+
+        setUser(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    async function getPermissions() {
+      try {
+        let res = await axios.get(
+          'http://localhost:3001/api/permissions/allPermissionsData'
+        );
+
+        setPermission(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getCategory();
+    getUsers();
+    getPermissions();
+  }, []);
+
+  const submit = async () => {
+    try {
+      let res = await axios.patch('http://localhost:3001/api/permissions', {
+        category: nowCategory,
+        user: nowUser,
+        permission: nowPermission,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="background ">
       <div className="permissions">
-        <div>
+        <div className="container">
           <div className="select"> 單位:</div>
-          <select>
-            <option> 單位a </option>
-          </select>
+          <CategoryFilter category={category} setNowCategory={setNowCategory} />
         </div>
-        <div>
+        <div className="container">
           <div className="select"> 使用者:</div>
-          <select>
-            <option> 曾子瑜 </option>
-          </select>
+          <UserFilter user={user} setNowUser={setNowUser} />
         </div>
-        <div>
+        <div className="container">
           <div className="select"> 權限:</div>
-          <select>
-            <option> 處理者 </option>
-          </select>
+          <PermissionFilter setNowPermission={setNowPermission} />
         </div>
 
-        <div className="btn">加入權限</div>
+        <div className="btn" onClick={submit}>
+          加入權限
+        </div>
       </div>
-      <div className="container">
+      <div className="container1">
         <div className="row mt">
           <div className="col-2 ">單位</div>
           <div className="col-3">使用者</div>
           <div className="col-5">權限</div>
         </div>
         <div className="vector"></div>
-        <div className="row mt">
-          <div className="col-2">單位</div>
-          <div className="col-3">使用者</div>
-          <div className="col-5 row getPermissions">
-            <div className="col-2">權限</div>
-            <div className="col-2">權限</div>
-          </div>
-          <div className="col-2">更改權限</div>
-        </div>
+        {permission.map((v, i) => {
+          const { applicant_unit, name, director, handler } = v;
+
+          return (
+            <div key={i} className="row mt">
+              <div className="col-2">{applicant_unit}</div>
+              <div className="col-3">{name}</div>
+              <div className="col-4 row getPermissions">
+                <div className="col-2">{director}</div>
+                <div className="col-2">{handler}</div>
+              </div>
+              <div className="col-2 btn">更改權限</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
