c24e27a71206104b4985555a5f6206d7955b0b28
TSENG-TZU-YU <46403343+TSENG-TZU-YU@users.noreply.github.com>
2023-01-30 11:15:47 +0800

    update

--- src/Audit/index.js
+++ src/Audit/index.js
@@ -7,54 +7,9 @@ import './_index.scss';
 import axios from 'axios';
 
 function Audit() {
-  const [user, setUser] = useState([]);
-  const [nowUser, setNowUser] = useState([]);
-  const [category, setCategory] = useState([]);
-  const [nowCategory, setNowCategory] = useState([]);
-  const [nowPermission, setNowPermission] = useState([]);
   const [audit, setAudit] = useState([]);
 
-  // const [permission, setPermission] = useState([
-  //   { applicant_unit: '國會', name: '曾子瑜', permissions: '處理者' },
-  //   { applicant_unit: '國會', name: '曾子瑜', permissions: '主管' },
-  //   { applicant_unit: '服務處A', name: 'Kelly', permissions: '處理者' },
-  //   { applicant_unit: '服務處B', name: '林鈺珊', permissions: '主管' },
-  // ]);
-
   useEffect(() => {
-    async function getCategory() {
-      try {
-        let res = await axios.get(
-          `${process.env.REACT_APP_BASE_URL}/api/permissions/category`
-        );
-
-        setCategory(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    async function getUsers() {
-      try {
-        let res = await axios.get(
-          `${process.env.REACT_APP_BASE_URL}/api/permissions/user`
-        );
-
-        setUser(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    async function getPermissions() {
-      try {
-        let res = await axios.get(
-          `${process.env.REACT_APP_BASE_URL}/api/permissions/allPermissionsData`
-        );
-
-        // setPermission(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    }
     async function audit() {
       try {
         let res = await axios.get(
@@ -66,26 +21,8 @@ function Audit() {
         console.log(err);
       }
     }
-    getCategory();
-    getUsers();
-    // getPermissions();
     audit();
   }, []);
-
-  const submit = async () => {
-    try {
-      let res = await axios.patch(
-        `${process.env.REACT_APP_BASE_URL}/api/permissions`,
-        {
-          category: nowCategory,
-          user: nowUser,
-          permission: nowPermission,
-        }
-      );
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   return (
     <div className="permissionsContainer">
